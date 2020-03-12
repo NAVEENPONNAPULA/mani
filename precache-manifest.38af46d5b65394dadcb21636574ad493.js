d40a582f827357d6ca01f4977c538126c6055265
NAVEENPONNAPULA <naveenponnapula53091@gmail.com>
2020-03-12 15:40:26 +0530

    Updates

---
+++ precache-manifest.38af46d5b65394dadcb21636574ad493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b612f9f3959867942f3ab82315771f13",
+    "url": "/mani/index.html"
+  },
+  {
+    "revision": "67ff64240365d547eccf",
+    "url": "/mani/static/css/main.ff71abd0.chunk.css"
+  },
+  {
+    "revision": "1226a9d2801e25ef4fd2",
+    "url": "/mani/static/js/2.e149ec31.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mani/static/js/2.e149ec31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67ff64240365d547eccf",
+    "url": "/mani/static/js/main.af6a32d5.chunk.js"
+  },
+  {
+    "revision": "ec4c390dca1a33d9895b",
+    "url": "/mani/static/js/runtime-main.d5534c5b.js"
+  },
+  {
+    "revision": "7c73eb4e72c5956e7168bf6bebbb8cfc",
+    "url": "/mani/static/media/vvit.7c73eb4e.jpeg"
+  }
+]);
